26eda2cc71adfb662e92fd4360d07ce62e62341b
Prakul H N <112687428+prakulhn@users.noreply.github.com>
2023-12-16 12:13:06 +0530

    Create quiz_database.py

---
+++ quiz_database.py
@@ -0,0 +1,15 @@
+question_bank = [
+        {'text': 'Who is the Father of our Nation?', 'ans': 'A'},
+        {'text': 'Who was the first President of India?', 'ans': 'C'},
+        {'text': 'Who is known as Father of Indian Constitution?', 'ans': 'B'},
+        {'text': 'Which is the most sensitive organ in our body?', 'ans': 'D'},
+        {'text': 'Which is the longest river on the earth?', 'ans': 'A'}
+    ]
+
+options = [
+        ['A. Mahatma Gandhi', 'B. Dr. Rajendra Prasad', 'C. Dr. B. R. Ambedkar', 'D. Charles Babbage'],
+        ['A. Mahatma Gandhi', 'B. Dr. B. R. Ambedkar', 'C. Dr. Rajendra Prasad', 'D. Charles Babbage'],
+        ['A. Mahatma Gandhi', 'B. Dr. B. R. Ambedkar', 'C. Dr. Rajendra Prasad', 'D. Charles Babbage'],
+        ['A. Nose', 'B. Ear', 'C. Tongue', 'D. Skin'],
+        ['A. Nile', 'B. Amazon', 'C. Ganga', 'D. Kaveri']
+    ]
